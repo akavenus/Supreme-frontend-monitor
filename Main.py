bb6a8b67939581aeaba85333bf06ad4a9f2997cf
akaanthony <71657809+akaanthony@users.noreply.github.com>
2020-11-01 16:20:34 -0500

    50

--- Main.py
+++ Main.py
@@ -1,23 +1,26 @@
 #To do
 #Add proxy support
 #Add more loops to monitor multiple products
-#Make the discord alert message contain more details about the product. Such as product name, product price, product image
+#Make the discord alert message contain more details about the product. Such as the product image
 
 import discord
 import requests
 from bs4 import BeautifulSoup
 client = discord.Client()
-
 url = 'https://www.supremenewyork.com/shop/accessories/t1e9xord8/bxd1pzhyu'
-response = requests.get(url).text
-soup = BeautifulSoup(response, 'lxml')
 x = True
 while x:
     try:
         if soup.find('input',{'class':'button'}):
+            response = requests.get(url).text
+            soup = BeautifulSoup(response, 'lxml') 
             price = soup.find()
             print("in stock")
             price_x = soup.find('p', class_='price').text
+            name_x = soup.find('h2', class_='protect').text
+            image_tags = soup.findAll('img')
+            for image_tag in image_tags:
+                print(image_tag.get('src'))
             x = False
             xa = "Send"
             @client.event
@@ -25,12 +28,14 @@ while x:
                 if xa == "Send":
                     text_channel = client.get_channel(770388223448449035)
                     myEmbed = discord.Embed(title="Supreme Restock ",  color=0x0000FF)
-                    myEmbed.add_field(name="Link", value=url,  inline=False)
+                    myEmbed.add_field(name="Name", value=name_x,  inline=False)
                     myEmbed.add_field(name="Price", value=price_x,  inline=False)
+                    myEmbed.add_field(name="Link", value=url,  inline=False)
                     await text_channel.send(embed=myEmbed)
                     
     except:
-        pass
+        print("error")
+        x = False
        
 
     
