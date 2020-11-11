30747d72627e6b489b2de96266d6c96eb3d506e6
akaanthony <71657809+akaanthony@users.noreply.github.com>
2020-11-11 14:21:39 -0500

    50

--- Supreme-monitor.py
+++ Supreme-monitor.py
@@ -3,35 +3,33 @@
 #Add more loops to monitor multiple products
 #Make the discord alert message contain more details about the product. Such as the product image
 
-import discord
 import requests
 from bs4 import BeautifulSoup
-client = discord.Client()
-url = 'https://www.supremenewyork.com/shop/accessories/t1e9xord8/bxd1pzhyu'
+from dhooks import Webhook, Embed
+url = 'https://www.supremenewyork.com/shop/accessories/g7u54g3ja/bxd1pzhyu'
+response = requests.get(url).text
+soup = BeautifulSoup(response, 'lxml') 
 x = True
 while x:
     try:
         if soup.find('input',{'class':'button'}):
-            response = requests.get(url).text
-            soup = BeautifulSoup(response, 'lxml') 
+            price = soup.find()
             print("in stock")
             price_x = soup.find('p', class_='price').text
             name_x = soup.find('h2', class_='protect').text
             x = False
-            xa = "Send"
-            @client.event
-            async def on_ready():
-                if xa == "Send":
-                    text_channel = client.get_channel(770388223448449035)
-                    myEmbed = discord.Embed(title="Supreme Restock ",  color=0x0000FF)
-                    myEmbed.add_field(name="Name", value=name_x,  inline=False)
-                    myEmbed.add_field(name="Price", value=price_x,  inline=False)
-                    myEmbed.add_field(name="Link", value=url,  inline=False)
-                    await text_channel.send(embed=myEmbed)
-                    
+            embed = Embed(
+            description='',color=0x5CDBF0,
+            timestamp='now'  # sets the timestamp to current time
+            )
+            hook = Webhook("enter your webhook here")
+            embed.set_author(name='Twitter Monitor')
+            embed.add_field(name="Name", value=name_x)
+            embed.add_field(name="Price", value=price_x)
+            embed.add_field(name="Link", value=url)
+            embed.set_footer(text='This bot was coded by Venus#9210')
+            hook.send(embed=embed)
     except:
-        pass
+        print("error finding product")
+        x = False
        
-
-    
-client.run('enter your token here')
