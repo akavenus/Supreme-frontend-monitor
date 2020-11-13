51f3bb2ae08f3fa8f02ad2fd1c6f87d15c1389bd
akavenus <71657809+akavenus@users.noreply.github.com>
2020-11-12 21:42:40 -0500

    50

--- Supreme-monitor.py
+++ Supreme-monitor.py
@@ -1,35 +1,43 @@
-#To do
-#Add proxy support
-#Add more loops to monitor multiple products
-#Make the discord alert message contain more details about the product. Such as the product image
-
 import requests
 from bs4 import BeautifulSoup
 from dhooks import Webhook, Embed
+import time
+from fake_headers import Headers
+headers = Headers(os="mac", headers=True).generate() #os is the operating system you want the header to be
 url = 'https://www.supremenewyork.com/shop/accessories/g7u54g3ja/bxd1pzhyu'
-response = requests.get(url).text
+Delay = 5 #Delay between each requests that you send to monitor the site
+response = requests.get(url, headers=headers).text
 soup = BeautifulSoup(response, 'lxml') 
 x = True
 while x:
     try:
         if soup.find('input',{'class':'button'}):
-            price = soup.find()
             print("in stock")
             price_x = soup.find('p', class_='price').text
             name_x = soup.find('h2', class_='protect').text
+            picture = soup.find(id='img-main')
+
+            film_id = 'img-main'
+            raw_picture = soup.find(itemprop="image")
+            picture_x = 'https:' + (raw_picture["src"])
+            print(picture_x)
             x = False
-            embed = Embed(
-            description='',color=0x5CDBF0,
-            timestamp='now'  # sets the timestamp to current time
-            )
-            hook = Webhook("enter your webhook here")
-            embed.set_author(name='Twitter Monitor')
-            embed.add_field(name="Name", value=name_x)
+            embed = Embed(title='Supreme Restock', description='', color=0x5CDBF0, timestamp='now')
+            hook = Webhook("https://discord.com/api/webhooks/775197355234426900/nbFhxu4gWP8dPrenMIf_xzSBxiHHkaMKGn6Tx3KTTHiLmQH-XWkBGtG2hugrxxScLKjs")
+            embed.set_author(name='Supreme monitor')
+            embed.set_image(picture_x)
+            embed.add_field(name="Product", value=name_x)
             embed.add_field(name="Price", value=price_x)
             embed.add_field(name="Link", value=url)
             embed.set_footer(text='This bot was coded by Venus#9210')
             hook.send(embed=embed)
+            time.sleep(Delay)
+
     except:
-        print("error finding product")
+        print("error finding product or out of stock")
+        time.sleep(Delay)
         x = False
        
+
+
+    
